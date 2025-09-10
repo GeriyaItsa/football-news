3c9f130419c3d0f83f6de5c8343386724a1a7ba4
GeriyaItsa <mgeriyaitsa@gmail.com>
2025-09-10 17:14:27 +0700

    final

--- football_news/settings.py
+++ football_news/settings.py
@@ -102,8 +102,8 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+            'NAME': BASE_DIR / 'db.sqlite3'
+        } 
     }
 
 
