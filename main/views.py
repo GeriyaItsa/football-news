4ed52da97b9d2db833ed620e49b8bb0ba9fce840
GeriyaItsa <mgeriyaitsa@gmail.com>
2025-10-01 16:00:54 +0700

    tes

--- main/views.py
+++ main/views.py
@@ -26,7 +26,7 @@ def show_main(request):
         news_list = News.objects.filter(user=request.user)
 
     context = {
-        'npm': '2406434172',
+        'npm': '240643417',
         'name': request.user.username,
         'class': 'PBP F',
         'news_list': news_list,
